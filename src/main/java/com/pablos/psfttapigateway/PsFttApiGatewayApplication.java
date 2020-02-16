e2535cb36f9ef25fadbec4bec9618292a1fafe9c
pawel <7proPS7@gmail.com>
2020-02-16 19:08:41 +0100

    PS-0000 - init API Gateway service

---
+++ src/main/java/com/pablos/psfttapigateway/PsFttApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pablos.psfttapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsFttApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsFttApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablos/psfttapigateway/PsFttApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablos.psfttapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsFttApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
